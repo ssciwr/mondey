f8eaaea02b1023a3d80de541850ae851754eb5ef
Liam Keegan <liam@keegan.ch>
2024-10-10 15:34:09 +0200

    Refactor localisation
    - User frontend
      - Default language is german with text stored in locales/de.json
        - This is part of the frontend build and so cannot be edited from the backend
      - All other languages are no longer part of the git repo
        - These are fetched as json files from the server
        - These can be edited from the backend
    - Admin frontend
      - Add interface for editing translations of frontend text
    - Backend
      - Add endpoint to update an 18n translation json file
    - resolves #79

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -1,18 +1,43 @@
-import { register, init } from 'svelte-i18n';
+import { init, addMessages } from 'svelte-i18n';
 import { languages } from '$lib/stores/langStore';
 import { getLanguages } from '$lib/client';
+import de from '../locales/de.json';
 
-register('de', () => import('../locales/de.json'));
-register('en', () => import('../locales/en.json'));
+export async function getI18nJson(lang_id: number) {
+	try {
+		const res = await fetch(`${import.meta.env.VITE_MONDEY_API_URL}/static/i18n/${lang_id}.json`);
+		if (!res.ok) {
+			console.log(
+				`getI18nJson failed for lang_id ${lang_id} with status ${res.status}, returning de translations`
+			);
+			return de;
+		}
+		return await res.json();
+	} catch {
+		console.log(`getI18nJson failed for lang_id ${lang_id}, returning de translations`);
+		return de;
+	}
+}
 
+async function getTranslation(lang: string, lang_id: number) {
+	const json = await getI18nJson(lang_id);
+	addMessages(lang, json);
+}
+
+export async function getTranslations() {
+	const { data, error } = await getLanguages();
+	if (!error && data) {
+		languages.set(data);
+		Object.entries(data).forEach(([lang, lang_id]) => {
+			if (lang_id !== 1) {
+				getTranslation(lang, lang_id);
+			}
+		});
+	}
+}
+
+addMessages('de', de);
 init({
 	fallbackLocale: 'de',
 	initialLocale: 'de'
 });
-
-export async function updateLanguages() {
-	const { data, error } = await getLanguages();
-	if (!error && data) {
-		languages.set(data);
-	}
-}
