0eff1dd223e5934700b4d310f4a289fd2ea00950
Harald Mack <harald.mack@protonmail.com>
2024-10-14 14:02:34 +0200

    reduce number of variables

--- frontend/src/lib/components/UserForgotPassword.svelte
+++ frontend/src/lib/components/UserForgotPassword.svelte
@@ -7,7 +7,6 @@
 	import { Button, Card, Heading, Input } from 'flowbite-svelte';
 	import { _ } from 'svelte-i18n';
 
-	const heading = $_('user.forgotPw.heading');
 	const maildata = {
 		component: Input,
 		type: 'text',
@@ -16,12 +15,8 @@
 			placeholder: $_('user.forgotPw.placeholder')
 		}
 	};
-	const successButtonLabel: string = $_('user.forgotPw.success');
-	const pendingButtonLabel: string = $_('user.forgotPw.pending');
-	const mailSentMessage: string = $_('user.forgotPw.mailSentMessage');
-	const alertTitle: string = $_('user.forgotPw.alertTitle');
 
-	let alertMessage: string = $_('user.forgotPw.alertMessage');
+	let alertMessage: string = $_('user.forgotPw.formatError');
 	let valid: boolean = true;
 	let showAlert: boolean = !valid;
 	let showSuccess: boolean = false;
@@ -36,33 +31,29 @@
 	}
 
 	async function submitData(mailstring: string): Promise<void> {
-		try {
-			const data = {
-				body: {
-					email: mailstring
-				},
-				throwOnError: true
-			};
-			const { response, error } = await resetForgotPassword(data);
+		const data = {
+			body: {
+				email: mailstring
+			},
+			throwOnError: true
+		};
 
-			if (error) {
-				console.log('error: ', error);
-				throw Error(error);
-			} else {
-				console.log('successful transmission, response status: ', response.status);
+		await resetForgotPassword(data)
+			.then((returned) => {
+				console.log('successful transmission, response status: ', returned.response.status);
 				showSuccess = true;
-			}
-		} catch (error) {
-			console.log(error);
-			showAlert = true;
-			alertMessage = $_('user.forgotPw.sendError');
-		}
+			})
+			.catch((error) => {
+				console.log(error);
+				showAlert = true;
+				alertMessage = $_('user.forgotPw.sendError');
+			});
 	}
 </script>
 
 {#if showAlert}
 	<AlertMessage
-		title={alertTitle}
+		title={$_('user.forgotPw.alertTitle')}
 		message={alertMessage}
 		lastpage={`${base}/userLand/lostPassword`}
 		onclick={() => {
@@ -75,7 +66,7 @@
 	<Heading
 		tag="h3"
 		class="m-2 p-2 text-center font-bold tracking-tight text-gray-700 dark:text-gray-400"
-		>{heading}</Heading
+		>{$_('user.forgotPw.heading')}</Heading
 	>
 	{#if showSuccess === false}
 		<div class="m-2 mx-auto w-full flex-col space-y-6 p-2">
@@ -95,19 +86,19 @@
 					if (valid) {
 						submitData(maildata.value);
 					}
-				}}>{pendingButtonLabel}</Button
+				}}>{$_('user.forgotPw.pending')}</Button
 			>
 		</div>
 	{:else}
 		<div class="m-2 flex w-full items-center justify-center p-2">
-			<p>{mailSentMessage}</p>
+			<p>{$_('user.forgotPw.mailSentMessage')}</p>
 		</div>
 		<div class="m-2 flex w-full items-center justify-center p-2">
 			<Button
 				size="md"
 				on:click={(event) => {
 					goto(`/${base}`);
-				}}>{successButtonLabel}</Button
+				}}>{$_('user.forgotPw.success')}</Button
 			>
 		</div>
 	{/if}
