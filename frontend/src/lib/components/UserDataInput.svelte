21a345538966158e8e2a2576b71218b0bbe6394d
Harald Mack <harald.mack@protonmail.com>
2024-10-17 16:48:48 +0200

    make more use of form functionality, add translations

--- frontend/src/lib/components/UserDataInput.svelte
+++ frontend/src/lib/components/UserDataInput.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { base } from '$app/paths';
 	import AlertMessage from '$lib/components/AlertMessage.svelte';
 	import DataInput from '$lib/components/DataInput/DataInput.svelte';
-	import NavigationButtons from '$lib/components/Navigation/NavigationButtons.svelte';
 	import { users, type UserData } from '$lib/stores/userStore';
-	import { Card, Heading } from 'flowbite-svelte';
-	import { afterUpdate, onDestroy, onMount } from 'svelte';
+	import { preventDefault } from '$lib/util';
+	import { Button, Card, Heading } from 'flowbite-svelte';
+	import { _ } from 'svelte-i18n';
 
 	function validate(): boolean {
 		missingValues = data.map((element) => element.value === '' || element.value === null);
 		return missingValues.every((v) => v === false);
 	}
 
-	async function acceptData() {
+	async function submitData() {
 		const valid = validate();
 		if (valid) {
 			for (let i = 0; i < data.length; ++i) {
@@ -39,56 +38,8 @@
 	let userData: UserData;
 	let userID: string;
 
-	onMount(async () => {
-		console.log('loading users');
-		await users.load();
-		userID = users.get()['loggedIn'] as string;
-		userData = users.get()[userID] as UserData;
-
-		// initialize data values to stuff that is there already if
-		// data has been supplied already for that user.
-		const keys = [
-			'Geburtsjahr',
-			'Geschlecht',
-			'Höchster Bildungsabschluss',
-			'Arbeitszeit/Woche',
-			'Familieneinkommen/Jahr',
-			'Beruf'
-		];
-		let allThere: boolean = false;
-		for (let i = 0; i < data.length; ++i) {
-			if (userData[keys[i]]) {
-				data[i]['value'] = userData[keys[i]].value;
-				data[i]['additionalValue'] = userData[keys[i]].additionalValue;
-
-				if (data[i].props['selected']) {
-					data[i].props['selected'] = data[i].props.items.map((e) => {
-						return e.value === userData[keys[i]].value;
-					});
-				}
-			} else {
-				allThere = true;
-			}
-		}
-		showAlert = allThere;
-		console.log('loaded data: ', data);
-		buttons[0].label = 'Fertig';
-	});
-
-	onDestroy(async () => {
-		users.save();
-	});
-
-	afterUpdate(async () => {
-		users.save();
-	});
-
 	// this can, but does not have to, come from a database later.
 	export let data: any[];
-
-	// validation / data acceptance
-
-	const heading = 'Benutzerdaten eingeben';
 
 	let missingValues = data.map(() => false);
 
@@ -108,10 +59,8 @@
 <!-- Show big alert message when something is missing -->
 {#if showAlert}
 	<AlertMessage
-		title="Fehler"
-		message={alertMessage}
-		infopage={`${base}/info`}
-		infotitle="Was passiert mit den Daten"
+		title={$_('userData.alertMessageTitle')}
+		message={$_('userData.alertMessageMissing')}
 		onclick={() => {
 			showAlert = false;
 		}}
@@ -121,15 +70,13 @@
 <!-- The actual content -->
 <div class="container m-1 mx-auto w-full max-w-xl">
 	<Card class="container m-1 mx-auto w-full max-w-xl">
-		{#if heading}
-			<Heading
-				tag="h3"
-				class="m-1 mb-3 p-1 text-center font-bold tracking-tight text-gray-700 dark:text-gray-400"
-				>{heading}</Heading
-			>
-		{/if}
+		<Heading
+			tag="h3"
+			class="m-1 mb-3 p-1 text-center font-bold tracking-tight text-gray-700 dark:text-gray-400"
+			>{$_('userData.heading')}</Heading
+		>
 
-		<form class="m-1 mx-auto w-full flex-col space-y-6">
+		<form class="m-1 mx-auto w-full flex-col space-y-6" onsubmit={preventDefault(submitData)}>
 			{#each data as element, i}
 				<DataInput
 					component={element.component}
@@ -161,6 +108,9 @@
 				/>
 			{/each}
 		</form>
-		<NavigationButtons {buttons} />
+		<Button
+			class="dark:bg-primay-700 bg-primary-700 hover:bg-primary-800 dark:hover:bg-primary-800 w-full text-center text-sm text-white hover:text-white"
+			type="submit">{$_('userData.submitButtonLabel')}</Button
+		>
 	</Card>
 </div>
