734c39619091b1941432d6894ad60307eb334731
Harald Mack <harald.mack@protonmail.com>
2024-11-07 14:39:32 +0100

    remove default questions b/c we decided on handling `child` data differently.

--- mondey_backend/src/mondey_backend/databases/milestones.py
+++ mondey_backend/src/mondey_backend/databases/milestones.py
@@ -6,8 +6,6 @@ from sqlmodel import create_engine
 from sqlmodel import select
 
 from ..models.milestones import Language
-from ..models.questions import ChildQuestion
-from ..models.questions import ChildQuestionText
 from ..settings import app_settings
 
 engine = create_engine(
@@ -23,95 +21,4 @@ def create_database():
         if session.exec(select(Language)).first() is None:
             session.add(Language(id="de"))
             session.add(Language(id="en"))
-            session.commit()
-
-        if session.exec(select(ChildQuestion)).first() is None:
-            session.add(
-                ChildQuestion(
-                    id=1,
-                    order=0,
-                    component="textarea",
-                    type="text",
-                    text={
-                        "de": ChildQuestionText(
-                            child_question_id=1,
-                            lang_id="de",
-                            question="Name des Kindes?",
-                            options="",
-                        ),
-                        "en": ChildQuestionText(
-                            child_question_id=1,
-                            lang_id="en",
-                            question="Child'Name?",
-                            options="",
-                        ),
-                    },
-                )
-            )
-            session.add(
-                ChildQuestion(
-                    id=2,
-                    order=1,
-                    component="date",
-                    type="input",
-                    text={
-                        "de": ChildQuestionText(
-                            child_question_id=2,
-                            lang_id="de",
-                            question="Geburtstag des Kindes?",
-                            options="",
-                        ),
-                        "en": ChildQuestionText(
-                            child_question_id=2,
-                            lang_id="en",
-                            question="Child's birthday?",
-                            options="",
-                        ),
-                    },
-                )
-            )
-            session.add(
-                ChildQuestion(
-                    id=3,
-                    order=2,
-                    component="textarea",
-                    type="text",
-                    text={
-                        "de": ChildQuestionText(
-                            child_question_id=3,
-                            lang_id="de",
-                            question="Bemerkungen",
-                            options="",
-                        ),
-                        "en": ChildQuestionText(
-                            child_question_id=3,
-                            lang_id="en",
-                            question="Remarks",
-                            options="",
-                        ),
-                    },
-                )
-            )
-            session.add(
-                ChildQuestion(
-                    id=4,
-                    order=3,
-                    component="fileupload",
-                    type="input",
-                    text={
-                        "de": ChildQuestionText(
-                            child_question_id=4,
-                            lang_id="de",
-                            question="Bild hochladen",
-                            options="",
-                        ),
-                        "en": ChildQuestionText(
-                            child_question_id=4,
-                            lang_id="en",
-                            question="Upload image",
-                            options="",
-                        ),
-                    },
-                )
-            )
             session.commit()
