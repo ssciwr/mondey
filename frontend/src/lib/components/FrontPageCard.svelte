6735592749298c630812ac8a019b8c2e4d52e100
Harald Mack <harald.mack@protonmail.com>
2024-11-15 14:56:09 +0100

    adjust styling of frontpagecard

--- frontend/src/lib/components/FrontPageCard.svelte
+++ frontend/src/lib/components/FrontPageCard.svelte
@@ -4,7 +4,7 @@ import { Button, Card, Tooltip } from "flowbite-svelte";
 import { _ } from "svelte-i18n";
 </script>
 
-<Card class="flex justify-center mr-4 text-center max-w-prose bg-additional-color min-w-full">
+<Card class="flex justify-center text-center max-w-prose bg-additional-color min-w-full">
   <h5 class="mb-2 mt-4 text-2xl font-bold text-gray-700 dark:text-white tracking-wide">{$_("frontpage.heading")}</h5>
   <p class="mb-5 text-base text-gray-600 sm:text-lg dark:text-gray-400">{$_("frontpage.summary")}</p>
   <div class="justify-center mb-1 items-center mt-2">
