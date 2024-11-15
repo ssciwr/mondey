3af32e423d22f194d53829d0bc984d242f7a2a50
Harald Mack <harald.mack@protonmail.com>
2024-11-15 14:20:44 +0100

    reorganize testable functionality  - most likely temporary

--- frontend/src/lib/components/DataDisplay/TableDisplay.svelte
+++ frontend/src/lib/components/DataDisplay/TableDisplay.svelte
@@ -5,46 +5,10 @@ import { TableBody, TableBodyRow, TableSearch } from "flowbite-svelte";
 import TableCell from "$lib/components/DataDisplay/TableElements/TableCell.svelte";
 import TableHeader from "$lib/components/DataDisplay/TableElements/TableHeader.svelte";
 
-function filterItemsDefault(
-	data: any,
-	searchTerm: string,
-	searchableColumns: string[],
-) {
-	// toString here for generality
-	return data.filter((item: any) =>
-		searchableColumns.some((column) =>
-			item[column]?.toString().includes(searchTerm),
-		),
-	);
-}
-
-function makePlaceholderTextDefault(data: any, searchableColumns: string[]) {
-	const numCols = Object.keys(data[0]).length;
-	let placeholderText = "Filter ";
-
-	if (searchableColumns.length === 1) {
-		placeholderText = placeholderText + searchableColumns[0];
-	} else if (
-		searchableColumns.length > 1 &&
-		searchableColumns.length <= numCols / 2
-	) {
-		placeholderText = `Filter any of ${searchableColumns.join(", ")}`;
-	} else if (
-		searchableColumns.length > numCols / 2 &&
-		searchableColumns.length < numCols
-	) {
-		const difference = Object.keys(data[0]).filter(
-			(key) => !searchableColumns.includes(key),
-		);
-		placeholderText = `Filter all columns except ${difference.join(", ")}`;
-	} else if (searchableColumns.length === numCols) {
-		placeholderText = "Filter all columns";
-	} else {
-		placeholderText = "Filter disabled";
-	}
-
-	return placeholderText;
-}
+import {
+	filterItemsDefault,
+	makePlaceholderTextDefault,
+} from "$lib/components/DataDisplay/TableElements/TableDisplayHelpers";
 
 let {
 	data = [],
---
+++ frontend/src/lib/components/DataDisplay/TableDisplayHelpers.test.ts
@@ -0,0 +1,86 @@
+import {
+	filterItemsDefault,
+	makePlaceholderTextDefault,
+} from "$lib/components/DataDisplay/TableDisplayHelpers";
+
+import { describe, expect, it } from "vitest";
+
+describe("filterItems: should return items in given columns ", () => {
+	const data = [
+		{ id: 1, name: "John", age: 25 },
+		{ id: 2, name: "Jane", age: 30 },
+		{ id: 3, name: "Bob", age: 28 },
+	];
+	const searchableColumns = ["name", "age"];
+
+	it("should return all items when the search term is empty", () => {
+		const searchTerm = "";
+		const result = filterItemsDefault(data, searchTerm, searchableColumns);
+		expect(result).toEqual(data);
+	});
+
+	it("should return no items when the search term has no overlap with entries of searchable columns", () => {
+		const searchTerm = 1;
+		const result = filterItemsDefault(data, searchTerm, searchableColumns);
+		expect(result).toEqual([]);
+	});
+
+	it("should return items case sensitively", () => {
+		const searchTermUpper = "Bob";
+		const searchTermLower = "bob";
+		const resultUpper = filterItemsDefault(
+			data,
+			searchTermUpper,
+			searchableColumns,
+		);
+		const resultLower = filterItemsDefault(
+			data,
+			searchTermLower,
+			searchableColumns,
+		);
+		expect(resultLower).toEqual([]);
+		expect(resultUpper).toEqual([{ id: 3, name: "Bob", age: 28 }]);
+	});
+
+	it("should return nothing if there are no searchable columns", () => {
+		const otherSearchableColumns = [] as string[];
+		const searchTerm = "Jane";
+		const result = filterItemsDefault(data, searchTerm, otherSearchableColumns);
+		expect(result).toEqual([]);
+	});
+});
+
+describe("makePlaceholderText: should return a placeholder text based on the number of searchable columns", () => {
+	const data = [
+		{ id: 1, name: "John", age: 25, income: 30000 },
+		{ id: 2, name: "Jane", age: 30, income: 35000 },
+		{ id: 3, name: "Bob", age: 28, income: 33000 },
+	];
+
+	it("should return colname when only one searchable columns", () => {
+		const searchableColumns = ["name"];
+		const result = makePlaceholderTextDefault(data, searchableColumns);
+		expect(result).toEqual("Filter name");
+	});
+	it("should return with except when searchable colnames are more than half of the colnames", () => {
+		const searchableColumns = ["name", "age", "income"];
+		const result = makePlaceholderTextDefault(data, searchableColumns);
+		expect(result).toEqual("Filter all columns except id");
+	});
+	it("should return with list when searchable colnames are less than half of the colnames", () => {
+		const searchableColumns = ["name", "age"];
+		const result = makePlaceholderTextDefault(data, searchableColumns);
+		expect(result).toEqual("Filter any of name, age");
+	});
+	it("should return with all when searchable colnames are less than half of the colnames", () => {
+		const searchableColumns = ["name", "age", "income", "id"];
+		const result = makePlaceholderTextDefault(data, searchableColumns);
+		expect(result).toEqual("Filter all columns");
+	});
+
+	it("should return with all when searchable colnames are less than half of the colnames", () => {
+		const searchableColumns = [];
+		const result = makePlaceholderTextDefault(data, searchableColumns);
+		expect(result).toEqual("Filter disabled");
+	});
+});
