e4ad2fc9a37189ae4019aa2752d2bc60af9a512a
Igor Zaripov <majapur1@gmail.com>
2024-11-15 08:22:22 +0100

    Changing the card gallery to one card on the main page (#145)
    add a card to the home page as a separate element

--- frontend/src/lib/components/Frontpage.svelte
+++ frontend/src/lib/components/Frontpage.svelte
@@ -4,70 +4,17 @@ import { base } from "$app/paths";
 
 import CardDisplay from "$lib/components//DataDisplay/CardDisplay.svelte";
 import GalleryDisplay from "$lib/components/DataDisplay/GalleryDisplay.svelte";
+import FrontPageCard from "$lib/components/FrontPageCard.svelte";
 
 export let getStarted = "";
 
-export let items = [
-	{
-		header: "Was ist Mondey?",
-		summary:
-			"Mondey ist ein wissentschaftlich geprüftes Programm zure Dokumentation der Entwicklung von Kindern bis 6 Jahren.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "Wozu ist Mondey gut?",
-		summary:
-			"Anhand ihrer Bewertungen der Fähigkeiten des Kindes erhalten sie Feedback zum Entwicklungsstand des Kindes und können so frühzeitig Fördermaßnahmen einleiten. Dies folgt einem übersichtlichen Ampelsystem.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "Was umfasst Mondey?",
-		summary:
-			"Mondey umfasst unterschiedliche Entwicklungsbereiche wie von Kindern im Alter von 0 bis 6 Jahren. Dazu gehören unter anderem Grob-und feinmotorik, Wahrnehmung, Denkne, Sprache und Soziale Beziehungen.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "Wie funktioniert Mondey?",
-		summary:
-			"Sie bewerten wie gut das Kind bestimmte Alltagshandlungen durchführen kann mit Hilfe einer Liste von Fragen.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "Wo fange ich an?",
-		summary:
-			"Um zu beginnen, müssen sie sich registrieren und ein Profil für ihr Kind anlegen.",
-		href: getStarted,
-		button: "Los geht´s",
-	},
-	{
-		header: "dummy?",
-		summary:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit repellendus distinctio facilis! Voluptas corrupti recusandae sapiente doloribus voluptatem fugiat ducimus.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "dummy?",
-		summary:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit repellendus distinctio facilis! Voluptas corrupti recusandae sapiente doloribus voluptatem fugiat ducimus.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-	{
-		header: "dummy?",
-		summary:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit repellendus distinctio facilis! Voluptas corrupti recusandae sapiente doloribus voluptatem fugiat ducimus.",
-		href: `${base}/info`,
-		button: "Mehr Info",
-	},
-];
+export let items = [];
 
 const props = {};
 </script>
+
+<FrontPageCard
+/>
 
 <GalleryDisplay
 	withSearch={false}
