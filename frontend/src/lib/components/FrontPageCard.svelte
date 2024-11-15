cfb1cb16fb5e2de0be7d69fa2790edb4a4886cea
Harald Mack <harald.mack@protonmail.com>
2024-11-15 14:43:00 +0100

    add functionality to frontpage button

--- frontend/src/lib/components/FrontPageCard.svelte
+++ frontend/src/lib/components/FrontPageCard.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+import { goto } from "$app/navigation";
 import { Button, Card, Tooltip } from "flowbite-svelte";
 import { _ } from "svelte-i18n";
 </script>
@@ -7,7 +8,9 @@ import { _ } from "svelte-i18n";
   <h5 class="mb-2 mt-4 text-2xl font-bold text-gray-700 dark:text-white tracking-wide">{$_("frontpage.heading")}</h5>
   <p class="mb-5 text-base text-gray-600 sm:text-lg dark:text-gray-400">{$_("frontpage.summary")}</p>
   <div class="justify-center mb-1 items-center mt-2">
-      <Button class="text-bold text-md w-44 hover:text-white dark:text-white bg-white text-gray-700">{$_("frontpage.buttonLabel")}</Button>
+      <Button class="text-bold text-md w-44 hover:text-white dark:text-white bg-white text-gray-700" onclick ={() => {
+        goto("/userLand/userRegistration");
+      }}>{$_("frontpage.buttonLabel")}</Button>
       <Tooltip>{$_("frontpage.toolTip")}</Tooltip>
   </div>
 </Card>
