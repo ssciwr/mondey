9a1d000e984ba822d4894c02c816b36e633ed491
Liam Keegan <liam@keegan.ch>
2024-11-20 10:51:01 +0100

    fix image import

--- frontend/src/lib/components/FrontPageAbout.svelte
+++ frontend/src/lib/components/FrontPageAbout.svelte
@@ -1,10 +1,11 @@
 <script>
+import babyFlower from "$lib/assets/babyFlower.jpg";
 import { Card } from "flowbite-svelte";
 import { _ } from "svelte-i18n";
 </script>
 
   <div class="flex mt-4 justify-center min-w-full">
-    <Card img="/lib/assets/babyFlower.jpg" horizontal size="full" class="shadow-none border-none">
+    <Card img={babyFlower} horizontal size="full" class="shadow-none border-none">
       <h5 class="mb-8 mt-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">{$_("frontpageAbout.heading")}</h5>
       <p class="mb-3 font-normal max-w-prose text-gray-700 dark:text-gray-400">{$_("frontpageAbout.summary1")}</p>
       <p class="mb-3 font-normal max-w-prose text-gray-700 dark:text-gray-400">{$_("frontpageAbout.summary2")}</p>
