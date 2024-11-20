52bb586d0e7f057f4c3dd7e995137f127c10c854
Majapur1 <majapur1@gmail.com>
2024-11-20 13:59:08 +0100

    fixing the alignment of elements on the front page

--- frontend/src/lib/components/FrontPageAbout.svelte
+++ frontend/src/lib/components/FrontPageAbout.svelte
@@ -4,10 +4,13 @@ import { Card } from "flowbite-svelte";
 import { _ } from "svelte-i18n";
 </script>
 
-  <div class="flex mt-4 justify-center min-w-full">
-    <Card img={babyFlower} horizontal size="full" class="shadow-none border-none">
-      <h5 class="mb-8 mt-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">{$_("frontpageAbout.heading")}</h5>
+  <div class="flex mt-6 justify-center min-w-full">
+    <Card horizontal class="max-w-4xl shadow-none border-none flex items-start space-x-6">
+      <img src={babyFlower} alt="{$_("frontpageAbout.alt")}" class="rounded-lg min-[768px]:w-1/3 max-[768px]:aspect-[16/8] max-[768px]:object-cover">
+      <div class="flex items-start flex-col">
+      <h5 class="mb-8 mt-5 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">{$_("frontpageAbout.heading")}</h5>
       <p class="mb-3 font-normal max-w-prose text-gray-700 dark:text-gray-400">{$_("frontpageAbout.summary1")}</p>
       <p class="mb-3 font-normal max-w-prose text-gray-700 dark:text-gray-400">{$_("frontpageAbout.summary2")}</p>
+      </div>
     </Card>
   </div>
--- frontend/src/lib/components/FrontPageCard.svelte
+++ frontend/src/lib/components/FrontPageCard.svelte
@@ -4,9 +4,10 @@ import { Button, Card, Tooltip } from "flowbite-svelte";
 import { _ } from "svelte-i18n";
 </script>
 
+<div class="flex justify-center px-4 mt-6">
 <Card class="flex justify-center text-center max-w-prose bg-additional-color min-w-full">
-  <h5 class="mb-2 mt-4 text-2xl font-bold text-gray-700 dark:text-white tracking-wide">{$_("frontpage.heading")}</h5>
-  <p class="mb-5 text-base text-gray-600 sm:text-lg dark:text-gray-400">{$_("frontpage.summary")}</p>
+  <h5 class="mb-2 mt-4 text-xl font-bold text-gray-700 dark:text-white tracking-wide">{$_("frontpage.heading")}</h5>
+  <p class="mb-5 text-base text-gray-600 sm:text-md dark:text-gray-400">{$_("frontpage.summary")}</p>
   <div class="justify-center mb-1 items-center mt-2">
       <Button class="text-bold text-md w-44 hover:text-white dark:text-white bg-white text-gray-700" onclick ={() => {
         goto("/userLand/userRegistration");
@@ -14,3 +15,4 @@ import { _ } from "svelte-i18n";
       <Tooltip>{$_("frontpage.toolTip")}</Tooltip>
   </div>
 </Card>
+</div>
