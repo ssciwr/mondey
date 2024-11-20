9ca59cf323252ddc265fabc315953c1235bff0d3
Igor Zaripov <majapur1@gmail.com>
2024-11-20 14:44:28 +0100

    Update frontend/src/lib/components/FrontPageAbout.svelte
    Co-authored-by: Liam Keegan <liam@keegan.ch>

--- frontend/src/lib/components/FrontPageAbout.svelte
+++ frontend/src/lib/components/FrontPageAbout.svelte
@@ -6,7 +6,7 @@ import { _ } from "svelte-i18n";
 
   <div class="flex mt-6 justify-center min-w-full">
     <Card horizontal class="max-w-4xl shadow-none border-none flex items-start space-x-6">
-      <img src={babyFlower} alt="{$_("frontpageAbout.alt")}" class="rounded-lg min-[768px]:w-1/3 max-[768px]:aspect-[16/8] max-[768px]:object-cover">
+      <img src={babyFlower} alt="{$_("frontpageAbout.alt")}" class="rounded-lg md:w-1/3 max-md:aspect-[16/8] max-md:object-cover">
       <div class="flex items-start flex-col">
       <h5 class="mb-8 mt-5 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">{$_("frontpageAbout.heading")}</h5>
       <p class="mb-3 font-normal max-w-prose text-gray-700 dark:text-gray-400">{$_("frontpageAbout.summary1")}</p>
