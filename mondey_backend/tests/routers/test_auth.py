3a6964cb8b455bcdefdf4979349c3d41191cf9b4
Liam Keegan <liam@keegan.ch>
2024-12-05 16:01:43 +0100

    Add email verification
    - docker-compose
      - add postfix image
    - backend
      - add SMTP_HOST to settings
      - send user an email with a verification link when they sign up
    - frontend
      - add /verify/[[code]] route
        - shows message & login page if verification successful, or account is already verified
        - shows error message otherwise
    - resolves #109

---
+++ mondey_backend/tests/routers/test_auth.py
@@ -0,0 +1,41 @@
+import smtplib
+from email.message import EmailMessage
+
+import pytest
+from fastapi.testclient import TestClient
+
+
+class SMTPMock:
+    last_message: EmailMessage | None = None
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+    def send_message(self, msg: EmailMessage, **kwargs):
+        SMTPMock.last_message = msg
+
+
+@pytest.fixture
+def smtp_mock(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(smtplib, "SMTP", SMTPMock)
+    return SMTPMock
+
+
+def test_register_new_user(public_client: TestClient, smtp_mock: SMTPMock):
+    assert smtp_mock.last_message is None
+    email = "u1@asdgdasf.com"
+    response = public_client.post(
+        "/auth/register", json={"email": email, "password": "p1"}
+    )
+    assert response.status_code == 201
+    msg = smtp_mock.last_message
+    assert msg is not None
+    assert "aktivieren" in msg.get("Subject").lower()
+    assert msg.get("To") == email
+    assert "/verify/" in msg.get_content()
