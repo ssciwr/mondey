6849f57654bea89187df58285ac45a9ca8746d25
jmsssc <sk353@uni-heidelberg.de>
2025-03-11 10:23:48 +0100

    Change home page to have log in / Register options (primary focus Register) initially.

--- frontend/src/lib/components/FrontPageCard.svelte
+++ frontend/src/lib/components/FrontPageCard.svelte
@@ -8,12 +8,19 @@ import { Button, Card, Tooltip } from "flowbite-svelte";
 <Card class="flex justify-center text-center max-w-prose bg-additional-color-400 min-w-full">
   <h5 class="mb-2 mt-4 text-xl font-bold text-gray-700 dark:text-white tracking-wide">{i18n.tr.frontpage.heading}</h5>
   <p class="mb-5 text-base text-gray-600 sm:text-md dark:text-gray-400">{i18n.tr.frontpage.summary}</p>
-  <div class="justify-center mb-1 items-center mt-2">
+  <div class="justify-center mb-1 items-center mt-2 space-x-2">
 
-      <Button class="text-bold text-md w-44 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white bg-white text-gray-700" onclick ={() => {
+
+        <Button class="text-bold text-md w-44 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white bg-white text-gray-700" onclick ={() => {
+        goto("/userLand/userLogin");
+        }}>
+            {i18n.tr.login.profileButtonLabelDefault}
+        </Button>
+
+      <Button class="text-bold text-md w-44 dark:text-white hover:dark:text-white text-gray-600 transition-colors hover:text-gray-900 max-lg:hover:bg-primary-800 dark:hover:text-gray-900 hover:bg-gray-100 hover:dark:bg-gray-800 bg-primary-600 text-white" onclick ={() => {
         goto("/signup");
       }}>{i18n.tr.frontpage.buttonLabel}</Button>
       <Tooltip>{i18n.tr.frontpage.toolTip}</Tooltip>
-  </div>
+    </div>
 </Card>
 </div>
