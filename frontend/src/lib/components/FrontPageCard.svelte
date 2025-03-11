ccf9eba423229dbe5bb91414de12c12c141cc329
jmsssc <sk353@uni-heidelberg.de>
2025-03-11 11:14:37 +0100

    Correct the margins so buttons on front page align vertically on mobile, and horizontally with a good margin on desktop

--- frontend/src/lib/components/FrontPageCard.svelte
+++ frontend/src/lib/components/FrontPageCard.svelte
@@ -8,14 +8,14 @@ import { Button, Card } from "flowbite-svelte";
 <Card class="flex justify-center text-center max-w-prose bg-additional-color-400 min-w-full">
   <h5 class="mb-2 mt-4 text-xl font-bold text-gray-700 dark:text-white tracking-wide">{i18n.tr.frontpage.heading}</h5>
   <p class="mb-5 text-base text-gray-600 sm:text-md dark:text-gray-400">{i18n.tr.frontpage.summary}</p>
-  <div class="justify-center mb-1 items-center mt-2 space-x-2">
+  <div class="justify-center mb-1 items-center mt-2">
 
 
-      <Button class="text-bold text-md w-44 mb-2 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white bg-white text-gray-700" onclick ={() => {
+      <Button class="text-bold text-md w-44 mb-2 m-1 dark:bg-gray-100 hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white bg-white text-gray-700" onclick ={() => {
         goto("/signup");
       }}>{i18n.tr.frontpage.buttonLabel}</Button>
 
-      <Button class="text-bold text-md w-44 dark:text-white transition-colors hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white  max-lg:hover:bg-primary-800  bg-primary-600 text-white" onclick ={() => {
+      <Button class="text-bold text-md w-44 m-1 dark:text-white transition-colors hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-white  max-lg:hover:bg-primary-800  bg-primary-600 text-white" onclick ={() => {
         goto("/userLand/userLogin");
         }}>
           {i18n.tr.login.profileButtonLabelDefault}
