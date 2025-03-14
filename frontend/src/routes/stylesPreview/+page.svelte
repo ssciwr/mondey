4d76426326e1f2e91a89bce4a4db316dc65c9f23
jmsssc <sk353@uni-heidelberg.de>
2025-03-14 14:13:11 +0100

    Change app wide CSS styles so we can use these classes for elements rather than lots of tailwind classes.

---
+++ frontend/src/routes/stylesPreview/+page.svelte
@@ -0,0 +1,63 @@
+<style>
+    div, button {
+        margin-bottom: 100px;
+    }
+</style>
+
+h1:
+<div>
+    <h1 class="h1">Lorem Ipsum Dolor Sit Amet</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis eget urna ultrices tincidunt.</p>
+</div>
+
+h2:
+<div>
+    <h2 class="h2">Consectetur Adipiscing Elit</h2>
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+</div>
+
+h3:
+<div>
+    <h3 class="h3">Nullam Eget Felis</h3>
+    <p>Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+</div>
+
+text-muted
+<div>
+    <h2 class="h2">Important Information</h2>
+    <p class="text-muted">Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+</div>
+
+btn-primary:
+<button class="btn-primary">
+    Submit Form
+</button>
+
+btn-secondary:
+<button class="btn-secondary">
+    Learn More
+</button>
+
+btn-danger:
+<button class="btn-danger">
+    Delete Item
+</button>
+
+<h1 class="h1">Layout examples</h1>
+panel:
+<div class="panel">
+    <div>
+        <h1 class="h1">Panel Layout Example</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+
+panel-centred:
+<div class="panel-centered">
+    <div>
+        <h1 class="h1">Centered Panel Example</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+        <button class="btn-secondary">Contact Us</button>
+    </div>
+</div>
