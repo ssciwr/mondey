75b59b93059ad902f92c99bf8ded455e71359573
jmsssc <sk353@uni-heidelberg.de>
2025-03-21 15:13:13 +0100

    Remove the +/- 6 month delta visibility from the milestones which display, as the milestones data isn't age specific which we are at least importing, so it seems that it won't be needed to limit it in the GUI. #263

--- mondey_backend/src/mondey_backend/routers/utils.py
+++ mondey_backend/src/mondey_backend/routers/utils.py
@@ -159,17 +159,7 @@ def get_or_create_current_milestone_answer_session(
             included_in_statistics=False,
         )
         add(session, milestone_answer_session)
-        delta_months = 6
-        child_age_months = get_child_age_in_months(child)
-        milestones = session.exec(
-            select(Milestone)
-            .where(
-                child_age_months >= col(Milestone.expected_age_months) - delta_months
-            )
-            .where(
-                child_age_months <= col(Milestone.expected_age_months) + delta_months
-            )
-        ).all()
+        milestones = session.exec(select(Milestone)).all()
         for milestone in milestones:
             session.add(
                 MilestoneAnswer(
