1c15ffafb8a8188a8bfdee8e7d8be2f060ec951a
jmsssc <sk353@uni-heidelberg.de>
2025-03-24 10:00:50 +0100

    Move import_data to the mondey_backend folder so we manage imports the same way as the rest of the backend code does; tests now import and connect.

--- mondey_backend/src/mondey_backend/databases/mondey.py
+++ mondey_backend/src/mondey_backend/databases/mondey.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from sqlalchemy.engine import Engine
 from sqlmodel import Session
 from sqlmodel import SQLModel
 from sqlmodel import create_engine
@@ -14,11 +15,17 @@ engine = create_engine(
 )
 
 
-def create_mondey_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def create_mondey_db_and_tables_themselves(provided_engine: Engine) -> None:
+    SQLModel.metadata.create_all(provided_engine)
+    with Session(provided_engine) as session:
         # add "de" and "en" Languages if no languages are present in the database:
         if session.exec(select(Language)).first() is None:
             session.add(Language(id="de"))
             session.add(Language(id="en"))
             session.commit()
+
+
+def create_mondey_db_and_tables(optional_engine: Engine | None = None) -> None:
+    create_mondey_db_and_tables_themselves(
+        engine if optional_engine is None else optional_engine
+    )
