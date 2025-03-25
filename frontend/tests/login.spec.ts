c31d78f3b0c6dc7444a04633cdd3d66a7b37d214
jmsssc <sk353@uni-heidelberg.de>
2025-03-25 10:31:13 +0100

    Move login spec test to better file

---
+++ frontend/tests/login.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "@playwright/test";
+
+test("/login : A non-existing user account cannot login", async ({ page }) => {
+	await page.goto("/login");
+
+	await page.fill("#username", "fakeUsername@test.com");
+	await page.fill("#password", "8n408sdnk2349");
+
+	await page.getByText("Absenden").click();
+
+	await expect(page.getByText(/Fehler/i)).toBeVisible({ timeout: 60000 });
+	await expect(
+		page.getByText(/Ungültige E-Mail-Adresse oder ungültiges/i),
+	).toBeVisible();
+	await expect(page.getByText(/LOGIN_BAD_CREDENTIALS/i)).toHaveCount(0);
+});
