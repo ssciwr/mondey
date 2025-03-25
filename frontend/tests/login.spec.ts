fabc4f8f15247d13de2866965c1f3a4f2f0ac9d1
jmsssc <sk353@uni-heidelberg.de>
2025-03-25 14:07:27 +0100

    Remove timeout from login test assertion

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -7,7 +7,7 @@ test("/login : A non-existing user account cannot login", async ({ page }) => {
 	await page.fill("#password", "8n408sdnk2349");
 
 	await page.getByText("Absenden").click();
-	await expect(page.getByText(/Fehler/i)).toBeVisible({ timeout: 60000 });
+	await expect(page.getByText(/Fehler/i)).toBeVisible();
 	await expect(
 		page.getByText(/Ungültige E-Mail-Adresse oder ungültiges/i),
 	).toBeVisible();
