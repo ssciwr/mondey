4e0dd20ba65a03ed5278cb11761a1fff1d696159
jmsssc <sk353@uni-heidelberg.de>
2025-04-03 14:56:53 +0200

    293: Add tooltips to Edit and Delete buttons: Hovering over edit though, the tooltip makes the delte button shift to the right.

--- frontend/src/lib/components/Admin/DeleteButton.svelte
+++ frontend/src/lib/components/Admin/DeleteButton.svelte
@@ -2,6 +2,7 @@
 
 <script lang="ts">
 import { i18n } from "$lib/i18n.svelte";
+import { Tooltip } from "flowbite-svelte";
 import TrashBinOutline from "flowbite-svelte-icons/TrashBinOutline.svelte";
 import Button from "flowbite-svelte/Button.svelte";
 
@@ -9,3 +10,4 @@ let { onclick }: { onclick: (event: Event) => void } = $props();
 </script>
 
 <Button class="btn-danger btn-icon" aria-roledescription={i18n.tr.admin.delete} {onclick}><TrashBinOutline class="h-5 w-5" /></Button>
+<Tooltip class="btn-secondary btn-icon">{i18n.tr.admin.delete}</Tooltip>
--- frontend/src/lib/components/Admin/EditButton.svelte
+++ frontend/src/lib/components/Admin/EditButton.svelte
@@ -2,10 +2,13 @@
 
 <script lang="ts">
 import { i18n } from "$lib/i18n.svelte";
+import { Tooltip } from "flowbite-svelte";
 import EditOutline from "flowbite-svelte-icons/EditOutline.svelte";
 import Button from "flowbite-svelte/Button.svelte";
 
 let { onclick }: { onclick: (event: Event) => void } = $props();
 </script>
 
+
 <Button class="btn-secondary btn-icon" aria-roledescription={i18n.tr.admin.edit} {onclick}><EditOutline class="h-5 w-5" /></Button>
+<Tooltip class="btn-secondary btn-icon" style="margin-left:-10px">{i18n.tr.admin.edit}</Tooltip>
