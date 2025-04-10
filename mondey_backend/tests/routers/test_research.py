624d42b706854d36e9e137241b852a69031c06ee
jmsssc <sk353@uni-heidelberg.de>
2025-04-10 16:04:17 +0200

    Adapt tests to the changing API for  deleting children (needs explicit dry_run=false to actually delete children)

--- mondey_backend/tests/routers/test_research.py
+++ mondey_backend/tests/routers/test_research.py
@@ -35,7 +35,7 @@ def test_research_data_deleted_child(admin_client: TestClient):
     assert len(admin_client.get("/users/children/").json()) == 1
     assert len(admin_client.get("/research/data/").json()) == 3
     # if a child is deleted, their answers are no longer included
-    assert admin_client.delete("/users/children/3").status_code == 200
+    assert admin_client.delete("/users/children/3?dry_run=false").status_code == 200
     assert len(admin_client.get("/users/children/").json()) == 0
     assert len(admin_client.get("/research/data/").json()) == 2
 
