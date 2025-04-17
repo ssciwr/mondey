9815a8cc69e5469d02f4c05d49c9584f2562ae01
jmsssc <sk353@uni-heidelberg.de>
2025-04-17 11:32:54 +0200

    Make tests work and consider the differences between admin/normal user get_user_questions for admins. The same for child users.

--- mondey_backend/tests/routers/admin_routers/test_admin_questions.py
+++ mondey_backend/tests/routers/admin_routers/test_admin_questions.py
@@ -27,15 +27,17 @@ def test_visbility_hidden_questions_only_included_in_admin_endpoint(
     assert len(user_questions_with_invisible_question) == 3
     assert response.json() == user_questions_with_invisible_question
 
-    print("Resp JSON was: ")
-    print(response.json())
+    response_as_user = admin_client.get("/user-questions/")
+    assert response_as_user.status_code == 200
 
-    response = admin_client.get("/user-questions/")
-    assert response.status_code == 200
+    assert len(response_as_user.json()) == 2
+    assert len(user_questions) == 2
 
-    assert len(response.json()) == 2
-    assert response.json() == user_questions  # visible ones only
-    # Question 3 is not visible.
+    # Check that Question 3 (not visible) is not included in the response
+    for question in response_as_user.json():
+        assert question.get("id") != 3, (
+            "Question 3 should not be visible in the response"
+        )
 
 
 def test_create_user_question_works(admin_client: TestClient):
@@ -180,12 +182,27 @@ def test_delete_user_question_id_not_there(admin_client: TestClient):
     assert response.status_code == 404
 
 
-def test_get_child_question_admin_works(admin_client: TestClient, child_questions):
+def test_visbility_hidden_child_questions_only_included_in_admin_endpoint(
+    admin_client: TestClient, child_questions, child_questions_with_invisible_question
+):
     response = admin_client.get("/admin/child-questions/")
-
     assert response.status_code == 200
-    assert [element["order"] for element in response.json()] == [0, 1]
-    assert response.json() == child_questions
+
+    assert [element["order"] for element in response.json()] == [0, 1, 2]
+    assert len(child_questions_with_invisible_question) == 3
+    assert response.json() == child_questions_with_invisible_question
+
+    response_as_child = admin_client.get("/child-questions/")
+    assert response_as_child.status_code == 200
+
+    assert len(response_as_child.json()) == 2
+    assert len(child_questions) == 2
+
+    # Check that Question 3 (not visible) is not included in the response
+    for question in response_as_child.json():
+        assert question.get("id") != 3, (
+            "Question 3 should not be visible in the response"
+        )
 
 
 def test_create_child_question_works(admin_client: TestClient):
@@ -193,7 +210,7 @@ def test_create_child_question_works(admin_client: TestClient):
 
     assert response.status_code == 200
     assert response.json() == {
-        "id": 3,
+        "id": 4,
         "name": "",
         "order": 0,
         "component": "select",
@@ -202,21 +219,21 @@ def test_create_child_question_works(admin_client: TestClient):
         "text": {
             "de": {
                 "options_json": "",
-                "child_question_id": 3,
+                "child_question_id": 4,
                 "options": "",
                 "lang_id": "de",
                 "question": "",
             },
             "en": {
                 "options_json": "",
-                "child_question_id": 3,
+                "child_question_id": 4,
                 "options": "",
                 "lang_id": "en",
                 "question": "",
             },
             "fr": {
                 "options_json": "",
-                "child_question_id": 3,
+                "child_question_id": 4,
                 "options": "",
                 "lang_id": "fr",
                 "question": "",
@@ -304,7 +321,7 @@ def test_update_child_question_id_not_there(admin_client: TestClient):
 
 def test_delete_child_question_works(session, admin_client: TestClient):
     child_questions = session.exec(select(ChildQuestion)).all()
-    assert len(child_questions) == 2
+    assert len(child_questions) == 3
 
     child_answers = session.exec(select(ChildAnswer)).all()
     assert len(child_answers) == 2
@@ -317,7 +334,7 @@ def test_delete_child_question_works(session, admin_client: TestClient):
     assert response_json["children"]["affectedQuestionAnswers"] == 1
 
     child_questions = session.exec(select(ChildQuestion)).all()
-    assert len(child_questions) == 2
+    assert len(child_questions) == 3
 
     response = admin_client.delete(
         "/admin/child-questions/1?dry_run=false"
@@ -327,7 +344,7 @@ def test_delete_child_question_works(session, admin_client: TestClient):
     assert response_json["ok"]
 
     child_questions = session.exec(select(ChildQuestion)).all()
-    assert len(child_questions) == 1
+    assert len(child_questions) == 2
     assert child_questions[0].id == 2
 
     child_answers = session.exec(select(ChildAnswer)).all()
