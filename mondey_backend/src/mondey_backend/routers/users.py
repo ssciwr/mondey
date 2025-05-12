Fix milestone group svg/stddev calculation (#363)
* Fix milestone group svg/stddev calculation

- include all milestones in a group when calculating score avg/std dev for that group
  - if a milestone was not included in a milestone answer session
    - assume a score of 3 if the child is older than the milestone expected age
    - assume a score of 0 if the child is younger than the milestone expected age
  - this ensures we are comparing avg/std dev across the same populations
- update and extend tests
  - include stats session into main test session
    - otherwise makes it harder to ensure the pre-constructed statistics are correct as they depend on which answer sessions are present
- resolves #359

Improve how answer session progress is tracked and communicated

- add `completed` field to `MilestoneAnswerSession`
  - completed flag is set when the last remaining milestone answer for a session is provided
    - the expired flag is also set at the same time to prevent a completed session from being modified
    - remove no longer needed code to expire stale answer sessions when calculating stats - now we just filter on completed answer sessions
  - only completed sessions are used to calculate averages and feedback
  - user feedback is now only provided for completed answer sessions
  - response from milestone-answers endpoint now includes `session_complete` bool so that frontend can redirect to feedback page when answer session is complete
- add `active_answer_session`, `session_progress`, `session_remaining_seconds` for each child to `/children` endpoint
  - frontend uses these to display progress bar and time remaining for the current session for each child in overview
  - add luxon library to convert duration in seconds to localised text
- change answer session duration to 14 days
- resolves #347

Also

- update algorithm to calculate expected age based on current data
  - require at least 3 answers for each age
  - expected age is the first age where average

mondey_backend/src/mondey_backend/routers/users.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import logging
 from datetime import datetime
 
+import numpy as np
 from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi import Query
@@ -16,7 +18,9 @@ from ..dependencies import SessionDep
 from ..models.children import Child
 from ..models.children import ChildCreate
 from ..models.children import ChildPublic
+from ..models.children import ChildSummaryPublic
 from ..models.milestones import MilestoneAnswerPublic
+from ..models.milestones import MilestoneAnswerResponse
 from ..models.milestones import MilestoneAnswerSession
 from ..models.milestones import MilestoneAnswerSessionPublic
 from ..models.milestones import MilestoneGroupPublic
@@ -32,11 +36,13 @@ from .scores import compute_milestonegroup_feedback_detailed
 from .scores import compute_milestonegroup_feedback_summary
 from .utils import add
 from .utils import child_image_path
+from .utils import current_milestone_answer_session
 from .utils import get
 from .utils import get_childs_answering_sessions
 from .utils import get_db_child
 from .utils import get_milestonegroups_for_answersession
 from .utils import get_or_create_current_milestone_answer_session
+from .utils import session_remaining_seconds
 from .utils import write_image_file
 
 
@@ -45,12 +51,35 @@ def create_router() -> APIRouter:
     router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate))
 
     # children endpoints
-    @router.get("/children/", response_model=list[ChildPublic])
+    @router.get("/children/", response_model=list[ChildSummaryPublic])
     def get_children(session: SessionDep, current_active_user: CurrentActiveUserDep):
+        child_summaries: list[ChildSummaryPublic] = []
         children = session.exec(
             select(Child).where(col(Child.user_id) == current_active_user.id)
         ).all()
-        return children
+        for child in children:
+            milestone_answer_session = current_milestone_answer_session(
+                session, current_active_user, child
+            )
+            child_summary = ChildSummaryPublic.model_validate(
+                child,
+                update={
+                    "active_answer_session": False,
+                    "session_progress": 0,
+                    "session_remaining_seconds": 0,
+                },
+            )
+            if milestone_answer_session is not None:
+                child_summary.active_answer_session = True
+                answers = np.array(
+                    list(a.answer for a in milestone_answer_session.answers.values())
+                )
+                child_summary.session_progress = np.mean(answers >= 0, dtype=np.float64)
+                child_summary.session_remaining_seconds = session_remaining_seconds(
+                    milestone_answer_session
+                )
+            child_summaries.append(child_summary)
+        return child_summaries
 
     @router.get("/children/{child_id}", response_model=ChildPublic)
     def get_child(
@@ -178,7 +207,7 @@ def create_router() -> APIRouter:
 
     @router.put(
         "/milestone-answers/{milestone_answer_session_id}",
-        response_model=MilestoneAnswerPublic,
+        response_model=MilestoneAnswerResponse,
     )
     def update_milestone_answer(
         session: SessionDep,
@@ -198,7 +227,21 @@ def create_router() -> APIRouter:
             raise HTTPException(401)
         milestone_answer.answer = answer.answer
         session.commit()
-        return milestone_answer
+        session.refresh(milestone_answer_session)
+        logging.warning(milestone_answer_session)
+        if all(a.answer >= 0 for a in milestone_answer_session.answers.values()):
+            logging.warning(
+                sum(a.answer >= 0 for a in milestone_answer_session.answers.values())
+            )
+            milestone_answer_session.expired = True
+            milestone_answer_session.completed = True
+            session.add(milestone_answer_session)
+            session.commit()
+            session.refresh(milestone_answer_session)
+        return MilestoneAnswerResponse(
+            answer=MilestoneAnswerPublic.model_validate(milestone_answer),
+            session_completed=milestone_answer_session.completed,
+        )
 
     # Endpoints for answers to user question
     @router.get("/user-answers/", response_model=list[UserAnswerPublic])
@@ -279,7 +322,7 @@ def create_router() -> APIRouter:
         "/milestone-answers-sessions/{child_id}",
         response_model=dict[int, MilestoneAnswerSessionPublic],
     )
-    def get_expired_milestone_answer_sessions(
+    def get_completed_milestone_answer_sessions(
         session: SessionDep, current_active_user: CurrentActiveUserDep, child_id: int
     ) -> dict[int, MilestoneAnswerSessionPublic]:
         milestone_answer_sessions = {
@@ -288,7 +331,7 @@ def create_router() -> APIRouter:
                 select(MilestoneAnswerSession).where(
                     (col(MilestoneAnswerSession.user_id) == current_active_user.id)
                     & (col(MilestoneAnswerSession.child_id) == child_id)
-                    & col(MilestoneAnswerSession.expired)
+                    & col(MilestoneAnswerSession.completed)
                 )
             ).all()
         }
@@ -314,12 +357,7 @@ def create_router() -> APIRouter:
         answersession_id: int,
     ) -> dict[int, int]:
         answersession = get(session, MilestoneAnswerSession, answersession_id)
-        if answersession is None:
-            raise HTTPException(404, detail="Answer session not found")
-        child_id = answersession.child_id
-        feedback = compute_milestonegroup_feedback_summary(
-            session, child_id, answersession_id
-        )
+        feedback = compute_milestonegroup_feedback_summary(session, answersession)
         return feedback
 
     @router.get(
@@ -330,13 +368,8 @@ def create_router() -> APIRouter:
         session: SessionDep,
         answersession_id: int,
     ) -> dict[int, dict[int, int]]:
-        answersession = session.get(MilestoneAnswerSession, answersession_id)
-        if answersession is None:
-            raise HTTPException(404, detail="Answer session not found")
-        child_id = answersession.child_id
-        feedback = compute_milestonegroup_feedback_detailed(
-            session, child_id, answersession_id
-        )
+        answersession = get(session, MilestoneAnswerSession, answersession_id)
+        feedback = compute_milestonegroup_feedback_detailed(session, answersession)
         return feedback
 
     return router

mondey_backend/tests/utils/test_scores.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from sqlmodel import select
 
@@ -32,7 +33,6 @@ def test_compute_feedback_simple():
         age_months=8,
         avg_score=2.0,
         stddev_score=0.8,
-        expected_score=1.0,
     )
     score = 0
     assert compute_feedback_simple(dummy_scores, score) == -1
@@ -46,68 +46,71 @@ def test_compute_feedback_simple():
 
 @pytest.mark.asyncio
 async def test_compute_summary_milestonegroup_feedback_for_answersession_with_recompute(
-    statistics_session, user_session
+    session, user_session
 ):
     child_age = 8
-    # existing statistics for milestonegroup 1 at age 8 months: [0,1] -> mean = 0.5 +/- 0.5
-    statistics = statistics_session.exec(
+    existing_age_8_milestone_group1_scores = [1, 0, 0]  # existing statistics
+    statistics = session.exec(
         select(MilestoneGroupAgeScoreCollection).where(
             MilestoneGroupAgeScoreCollection.milestone_group_id == 1
         )
     ).all()
     assert len(statistics) == 1
-    assert statistics[0].scores[child_age].avg_score == pytest.approx(0.5, abs=0.001)
-    assert statistics[0].scores[child_age].stddev_score == pytest.approx(0.5, abs=0.001)
-    # answer session 1 scores: [0, 1] -> mean 0.5 -> green
+    assert statistics[0].scores[child_age].avg_score == pytest.approx(
+        np.mean(existing_age_8_milestone_group1_scores), abs=0.001
+    )
+    assert statistics[0].scores[child_age].stddev_score == pytest.approx(
+        np.std(existing_age_8_milestone_group1_scores), abs=0.001
+    )
+    # answer session 1 scores for milestone group 1: [1, 0, 0] -> mean 0.33 -> green
     feedback = compute_milestonegroup_feedback_summary(
-        statistics_session, child_id=1, answersession_id=1
+        session, session.get(MilestoneAnswerSession, 1)
     )
     assert feedback[1] == TrafficLight.green.value
     assert len(feedback) == 1
-    # answer session 2 scores: [1, 1] -> mean 1.0 -> green
+    # answer session 2 scores for milestone group 1: [1, 1, 0] -> mean 0.66 -> green
     feedback = compute_milestonegroup_feedback_summary(
-        statistics_session, child_id=1, answersession_id=2
+        session, session.get(MilestoneAnswerSession, 2)
     )
     assert feedback[1] == TrafficLight.green.value
     assert len(feedback) == 1
-    for update_existing_statistics in [True, False]:
+    for incremental_update in [True, False]:
         await async_update_stats(
-            statistics_session,
+            session,
             user_session,
-            incremental_update=update_existing_statistics,
+            incremental_update=incremental_update,
         )
-        # updated stats for milestonegroup 1 at age 8 months: [0,1,1,1,2,0] -> mean = 0.8333 +/- ~0.688
-        statistics = statistics_session.exec(
-            select(MilestoneGroupAgeScoreCollection).where(
-                MilestoneGroupAgeScoreCollection.milestone_group_id == 1
-            )
-        ).all()
-        assert len(statistics) == 1
-        assert statistics[0].scores[child_age].avg_score == pytest.approx(
-            0.83333, abs=0.001
+        updated_age_8_milestone_group1_scores = (
+            existing_age_8_milestone_group1_scores.copy()
         )
-        assert statistics[0].scores[child_age].stddev_score == pytest.approx(
-            0.688, abs=0.1
+        updated_age_8_milestone_group1_scores.extend([1, 1, 0])  # answer session 2
+        updated_age_8_milestone_group1_scores.extend([2, 0, 0])  # answer session 4
+        statistics = session.get(MilestoneGroupAgeScoreCollection, 1)
+        assert statistics.scores[child_age].avg_score == pytest.approx(
+            np.mean(updated_age_8_milestone_group1_scores), abs=0.001
+        )
+        assert statistics.scores[child_age].stddev_score == pytest.approx(
+            np.std(updated_age_8_milestone_group1_scores), abs=0.1
         )
         # answer session 1 score 0.5 -> remains green
         feedback = compute_milestonegroup_feedback_summary(
-            statistics_session, child_id=1, answersession_id=1
+            session, session.get(MilestoneAnswerSession, 1)
         )
         assert feedback[1] == TrafficLight.green.value
         assert len(feedback) == 1
-        # answer session 2 score 2.5 remain green
+        # answer session 2 score 1.5 -> remains green
         feedback = compute_milestonegroup_feedback_summary(
-            statistics_session, child_id=1, answersession_id=2
+            session, session.get(MilestoneAnswerSession, 2)
         )
         assert feedback[1] == TrafficLight.green.value
         assert len(feedback) == 1
 
 
 def test_compute_summary_milestonegroup_feedback_for_answersession_no_existing_stat(
-    statistics_session,
+    session,
 ):
     feedback = compute_milestonegroup_feedback_summary(
-        statistics_session, child_id=3, answersession_id=3
+        session, session.get(MilestoneAnswerSession, 3)
     )
 
     assert len(feedback) == 1
@@ -116,11 +119,11 @@ def test_compute_summary_milestonegroup_feedback_for_answersession_no_existing_s
 
 @pytest.mark.asyncio
 async def test_compute_detailed_milestonegroup_feedback_for_answersession_with_recompute(
-    statistics_session, user_session
+    session, user_session
 ):
     # initial stats only include answer session 1: all feedback green
     feedback = compute_milestonegroup_feedback_detailed(
-        statistics_session, child_id=1, answersession_id=1
+        session, session.get(MilestoneAnswerSession, 1)
     )
     assert len(feedback) == 1
     assert len(feedback[1]) == 2
@@ -128,9 +131,9 @@ async def test_compute_detailed_milestonegroup_feedback_for_answersession_with_r
     assert feedback[1][2] == TrafficLight.green.value
 
     # updated stats include more answer sessions
-    await async_update_stats(statistics_session, user_session, incremental_update=True)
+    await async_update_stats(session, user_session, incremental_update=True)
     feedback = compute_milestonegroup_feedback_detailed(
-        statistics_session, child_id=1, answersession_id=1
+        session, session.get(MilestoneAnswerSession, 1)
     )
     assert len(feedback) == 1
     assert len(feedback[1]) == 2
@@ -139,10 +142,10 @@ async def test_compute_detailed_milestonegroup_feedback_for_answersession_with_r
 
 
 def test_compute_detailed_milestonegroup_feedback_for_answersession_no_existing_stat(
-    statistics_session,
+    session,
 ):
     feedback = compute_milestonegroup_feedback_detailed(
-        statistics_session, child_id=3, answersession_id=3
+        session, session.get(MilestoneAnswerSession, 3)
     )
     assert len(feedback) == 1
     assert feedback[2][5] == TrafficLight.invalid.value

mondey_backend/tests/utils/test_statistics.py
@@ -94,23 +94,27 @@ def test_get_score_statistics_by_age(session):
         1: 5,
         2: 3,
         3: 8,
-        4: 11,
+        4: 5,
+        5: 17,
+        6: 28,
         99: 60,
     }
 
     count, avg, stddev = _get_statistics_by_age(answers, child_ages)
 
-    answers_5 = [answer.answer for answer in answers if answer.answer_session_id == 1]
+    answers_5 = [
+        answer.answer for answer in answers if answer.answer_session_id in [1, 4]
+    ]
     answers_3 = [answer.answer for answer in answers if answer.answer_session_id == 2]
     answers_8 = [answer.answer for answer in answers if answer.answer_session_id == 3]
 
-    assert count[5] == 2
+    assert count[5] == 4
     assert count[3] == 2
     assert count[8] == 1
 
-    assert np.isclose(avg[5], 0.5)
-    assert np.isclose(avg[3], (1 + 1) / 2.0)
-    assert np.isclose(avg[8], 2.0)
+    assert np.isclose(avg[5], np.mean(answers_5))
+    assert np.isclose(avg[3], np.mean(answers_3))
+    assert np.isclose(avg[8], np.mean(answers_8))
 
     assert np.isclose(
         stddev[5],
@@ -160,7 +164,7 @@ def test_get_score_statistics_by_age(session):
         second_answers, child_ages, count, avg, stddev
     )
 
-    assert count[5] == 4
+    assert count[5] == 8
     assert count[3] == 4
     assert count[8] == 2
     assert np.isclose(avg[5], np.mean(answers_5))
@@ -172,14 +176,22 @@ def test_get_score_statistics_by_age(session):
     assert np.isclose(stddev[8], np.std(answers_8, ddof=1))
 
 
-def test_get_score_statistics_by_age_no_data(statistics_session):
-    answers = statistics_session.exec(select(MilestoneAnswer)).all()
+def test_get_score_statistics_by_age_no_data(session):
+    answers = session.exec(select(MilestoneAnswer)).all()
     child_ages = {}  # no answer sessions ==> empty child ages
     count, avg, stddev = _get_statistics_by_age(answers, child_ages)
     assert np.all(np.isclose(avg, 0))
     assert np.all(np.isclose(stddev, 0))
 
-    child_ages = {1: 5, 2: 3, 3: 8, 4: 11, 99: 60}
+    child_ages = {
+        1: 5,
+        2: 3,
+        3: 8,
+        4: 11,
+        5: 17,
+        99: 60,
+        6: 28,
+    }
     answers = []  # no answers ==> empty answers
     count, avg, stddev = _get_statistics_by_age(answers, child_ages)
     assert np.all(count == 0)
@@ -188,9 +200,9 @@ def test_get_score_statistics_by_age_no_data(statistics_session):
 
 
 @pytest.mark.asyncio
-async def test_calculate_milestone_statistics_by_age(statistics_session, user_session):
-    m1 = statistics_session.get(MilestoneAgeScoreCollection, 1)
-    m2 = statistics_session.get(MilestoneAgeScoreCollection, 2)
+async def test_calculate_milestone_statistics_by_age(session, user_session):
+    m1 = session.get(MilestoneAgeScoreCollection, 1)
+    m2 = session.get(MilestoneAgeScoreCollection, 2)
 
     # existing stats (only answer session 1)
     assert m1.milestone_id == 1
@@ -204,9 +216,9 @@ async def test_calculate_milestone_statistics_by_age(statistics_session, user_se
     assert np.isclose(m2.scores[8].stddev_score, 0.0)
 
     # updated stats (answer sessions 1, 2, 4)
-    await async_update_stats(statistics_session, user_session, incremental_update=True)
-    m1 = statistics_session.get(MilestoneAgeScoreCollection, 1)
-    m2 = statistics_session.get(MilestoneAgeScoreCollection, 2)
+    await async_update_stats(session, user_session, incremental_update=True)
+    m1 = session.get(MilestoneAgeScoreCollection, 1)
+    m2 = session.get(MilestoneAgeScoreCollection, 2)
 
     assert m1.milestone_id == 1
     assert m1.scores[8].count == 3
@@ -219,9 +231,9 @@ async def test_calculate_milestone_statistics_by_age(statistics_session, user_se
     assert m2.scores[8].stddev_score == pytest.approx(0.577, abs=0.1)
 
     # re-calculating using all answers gives the same results
-    await async_update_stats(statistics_session, user_session, incremental_update=False)
-    m1 = statistics_session.get(MilestoneAgeScoreCollection, 1)
-    m2 = statistics_session.get(MilestoneAgeScoreCollection, 2)
+    await async_update_stats(session, user_session, incremental_update=False)
+    m1 = session.get(MilestoneAgeScoreCollection, 1)
+    m2 = session.get(MilestoneAgeScoreCollection, 2)
 
     assert m1.milestone_id == 1
     assert m1.scores[8].count == 3
@@ -235,32 +247,34 @@ async def test_calculate_milestone_statistics_by_age(statistics_session, user_se
 
 
 @pytest.mark.asyncio
-async def test_calculate_milestonegroup_statistics(statistics_session, user_session):
-    mg = statistics_session.get(MilestoneGroupAgeScoreCollection, 1)
+async def test_calculate_milestonegroup_statistics(session, user_session):
+    mg = session.get(MilestoneGroupAgeScoreCollection, 1)
 
     # existing stats (only answer session 1)
+    answers = [1, 0, 0]
     assert mg.milestone_group_id == 1
-    assert mg.scores[8].count == 2
-    assert np.isclose(mg.scores[8].avg_score, (0 + 1) / 2.0)
-    assert np.isclose(mg.scores[8].stddev_score, 0.5)
+    assert mg.scores[8].count == len(answers)
+    assert np.isclose(mg.scores[8].avg_score, np.mean(answers))
+    assert np.isclose(mg.scores[8].stddev_score, np.std(answers))
 
     # updated stats (answer sessions 1, 2, 4)
-    await async_update_stats(statistics_session, user_session, incremental_update=True)
-    mg = statistics_session.get(MilestoneGroupAgeScoreCollection, 1)
+    updated_answers = answers + [1, 1, 0] + [2, 0, 0]
+    await async_update_stats(session, user_session, incremental_update=True)
+    mg = session.get(MilestoneGroupAgeScoreCollection, 1)
 
     assert mg.milestone_group_id == 1
-    assert mg.scores[8].count == 6
-    assert np.isclose(mg.scores[8].avg_score, (0 + 1 + 1 + 1 + 2 + 0) / 6.0)
-    assert mg.scores[8].stddev_score == pytest.approx(0.719, abs=0.1)
+    assert mg.scores[8].count == len(updated_answers)
+    assert np.isclose(mg.scores[8].avg_score, np.mean(updated_answers))
+    assert mg.scores[8].stddev_score == pytest.approx(np.std(updated_answers), abs=0.1)
 
     # re-calculating using all answers gives the same results
-    await async_update_stats(statistics_session, user_session, incremental_update=False)
-    mg = statistics_session.get(MilestoneGroupAgeScoreCollection, 1)
+    await async_update_stats(session, user_session, incremental_update=False)
+    mg = session.get(MilestoneGroupAgeScoreCollection, 1)
 
     assert mg.milestone_group_id == 1
-    assert mg.scores[8].count == 6
-    assert np.isclose(mg.scores[8].avg_score, (0 + 1 + 1 + 1 + 2 + 0) / 6.0)
-    assert mg.scores[8].stddev_score == pytest.approx(0.719, abs=0.1)
+    assert mg.scores[8].count == len(updated_answers)
+    assert np.isclose(mg.scores[8].avg_score, np.mean(updated_answers))
+    assert mg.scores[8].stddev_score == pytest.approx(np.std(updated_answers), abs=0.1)
 
 
 def test_make_datatable_no_data():