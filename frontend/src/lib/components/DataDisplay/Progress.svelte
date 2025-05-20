cbc55c99ab773c10922bf0517dc366873559e5cf
jmsssc <sk353@uni-heidelberg.de>
2025-05-20 15:05:20 +0200

    Align progress bar colors to the color of the input

--- frontend/src/lib/components/DataDisplay/Progress.svelte
+++ frontend/src/lib/components/DataDisplay/Progress.svelte
@@ -5,16 +5,32 @@ let { progress = 0.0, color = "red" }: { progress: number; color: string } =
 	$props();
 
 let displayProgress = $derived(progress < 0.01 ? 1 : 100 * progress);
+
+const dynamicColorClass = `dynamic-bg-color-${id}`;
+
+let wrapperStyle = $derived(`--dynamic-bg-color-${id}: ${color};`);
+
+let labelClass = $derived(
+	progress === 100
+		? "text-white rounded-md text-small pl-2 bg-green"
+		: `text-black rounded-md text-small pl-2 ${dynamicColorClass}`,
+);
 </script>
 
-<div class="rounded-lg bg-white p-2 pb-4 w-full">
+<div class="rounded-lg bg-white p-2 pb-4 w-full" style={wrapperStyle}>
     <Progressbar
             animate={true}
-            color={displayProgress === 100 ? 'green' : 'red'}
             divClass={`h-full rounded-full w-${displayProgress}`}
             labelInside={true}
-            labelInsideClass={`${progress === 100 ? 'text-white' : 'text-black'} rounded-md text-small pl-2`}
+            labelInsideClass={labelClass}
             progress={`${displayProgress}`}
             size="h-4"
     />
 </div>
+
+<style>
+    /* Dynamic selector based on the instance ID */
+    :global(.dynamic-bg-color-progress-1) {
+        background-color: var(--dynamic-bg-color-progress-1);
+    }
+</style>
