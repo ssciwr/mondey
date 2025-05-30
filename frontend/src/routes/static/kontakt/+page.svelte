a029c522c5b1f86cb3e1e9c56f871230104ba909
jmsssc <sk353@uni-heidelberg.de>
2025-05-30 11:07:20 +0200

    Add static html pages

---
+++ frontend/src/routes/static/kontakt/+page.svelte
@@ -0,0 +1,12 @@
+<div class="p-10">
+    <h1 class="text-xl">Contact</h1>
+    <p>
+        Here are our contact details:
+    </p>
+    <p>
+        Call us on 01299999999
+    </p>
+
+    <h1 class="text-xl pt-10">Participate in the research</h1>
+    <p>Get in touch and <a href="somegoodform.com/lsdflsdflsdfl">register to participate and use Mondey</a></p>
+</div>
