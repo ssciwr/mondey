717eefe5a4a7a16201b37b8b5c1eb1ca8a74e82a
jmsssc <sk353@uni-heidelberg.de>
2025-08-08 13:45:31 +0200

    Fixes so that all tests pass. And improve the "Required question" appearance inline with guidance and a better translation

--- frontend/src/lib/components/DataInput/DataInput.svelte
+++ frontend/src/lib/components/DataInput/DataInput.svelte
@@ -75,18 +75,22 @@ let showTextField: boolean = $derived.by(
 let highlight = $derived(!valid && required === true);
 let derivedPlaceholder = $derived(
 	placeholder ??
-		(component === "select" ? i18n.tr.misc.selectPlaceholder : undefined),
+		(component === "select"
+			? required
+				? i18n.tr.misc.selectRequiredPlaceholder
+				: i18n.tr.misc.selectPlaceholder
+			: undefined),
 );
 </script>
 
 {#if label}
 	<Label
 			for={id}
-			class="font-semibold text-gray-700 dark:text-gray-400 {required && false === disabled ? 'border-l-2 border-additional-color-300 dark:border-additional-color-600 pl-2' : ''}"
+			class="font-semibold text-gray-700 dark:text-gray-400 {required && false === disabled ? 'border-l-2 border-additional-color-800 dark:border-additional-color-600 pl-2' : ''}"
 	>
 		{label}
 		{#if required && false === disabled}
-			&nbsp;* {i18n.tr.misc.selectPlaceholder}
+			&nbsp;* <small class="text-gray-500">{i18n.tr.misc.selectRequiredPlaceholder}</small>
 		{/if}
 	</Label>
 {/if}
