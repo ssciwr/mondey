c0bf7bea58c3f94e5418eb7170fe4398978d149b
jmsssc <sk353@uni-heidelberg.de>
2025-08-28 14:55:11 +0200

    Add .gitkeep file for alembic/versions + make env.py more dynamic

--- mondey_backend/alembic/env.py
+++ mondey_backend/alembic/env.py
@@ -6,26 +6,10 @@ from alembic import context
 from mondey_backend.databases.mondey import engine as mondey_engine
 
 # import all our models so that they are registered with SQLModel.metadata to allow alembic to autogenerate migrations
-from mondey_backend.models.children import Child
-from mondey_backend.models.milestones import AdminSettings
-from mondey_backend.models.milestones import Language
-from mondey_backend.models.milestones import Milestone
-from mondey_backend.models.milestones import MilestoneAgeScore
-from mondey_backend.models.milestones import MilestoneAgeScoreCollection
-from mondey_backend.models.milestones import MilestoneAnswer
-from mondey_backend.models.milestones import MilestoneAnswerSession
-from mondey_backend.models.milestones import MilestoneGroup
-from mondey_backend.models.milestones import MilestoneGroupAgeScore
-from mondey_backend.models.milestones import MilestoneGroupAgeScoreCollection
-from mondey_backend.models.milestones import MilestoneGroupText
-from mondey_backend.models.milestones import MilestoneText
-from mondey_backend.models.questions import ChildAnswer
-from mondey_backend.models.questions import ChildQuestion
-from mondey_backend.models.questions import ChildQuestionText
-from mondey_backend.models.questions import UserAnswer
-from mondey_backend.models.questions import UserQuestion
-from mondey_backend.models.questions import UserQuestionText
-from mondey_backend.models.research import ResearchGroup
+from mondey_backend.models.children import *
+from mondey_backend.models.milestones import *
+from mondey_backend.models.questions import *
+from mondey_backend.models.research import *
 
 # enable logging
 logging.basicConfig(level=logging.INFO)
