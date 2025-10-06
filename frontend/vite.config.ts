4cd24dc85f1eb175535ae523961d03d48ba633f2
jmsssc <sk353@uni-heidelberg.de>
2025-10-06 14:48:31 +0200

    Fix frontend loading of env (Default values revealed a problem)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig(({ mode }) => {
-	const host = process.env.VITE_HOST || "localhost";
-	const port = Number.parseInt(process.env.VITE_PORT || "5173", 10);
-	const apiUrl = process.env.VITE_API_PROXY_URL || "http://localhost:8000";
+	const env = loadEnv(mode, "../", "VITE_"); // because of this we could consolidate /frontend/.env into /.env too
+	const host = env.VITE_HOST || "localhost";
+	const port = Number.parseInt(env.VITE_PORT || "5173", 10);
+	const apiUrl = env.VITE_API_PROXY_URL || "http://localhost:8000";
 
 	return {
 		plugins: [sveltekit(), svelteTesting()],
