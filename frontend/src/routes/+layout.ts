1f799f0b26158e70b92bb8203347c488d4c5420c
jmsssc <sk353@uni-heidelberg.de>
2025-11-03 10:03:34 +0100

    Remove console log

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -2,7 +2,6 @@ import { client } from "$lib/client/client.gen";
 export const prerender = true;
 
 const apiUrl = import.meta.env.VITE_MONDEY_API_URL;
-console.log("Mondey API URL configured:", apiUrl);
 
 client.setConfig({
 	baseUrl: apiUrl,
