2807c8a5135b41315ea4b20cb9add71f5e0e6f7e
Liam Keegan <liam@keegan.ch>
2026-07-27 08:04:03 +0200

    Security improvements
    - update nginx config
      - bump image to latest stable version
      - remove some out of date security related config lines
      - add rate limiting to /auth endpoints
        - limit to a short burst of up to 10 requests
        - then average of 5 requests per minute
      - add limit to number of concurrent connections from a single address
    - require SECRET to be at least 20 characters long
    - require a verified email address for all authenticated access
    - sanitize user input before printing as html to document in frontend when generating report
    - make test account email filter more robust
    - also bump frontend node image to current LTS version 24

--- mondey_backend/src/mondey_backend/routers/auth.py
+++ mondey_backend/src/mondey_backend/routers/auth.py
@@ -23,7 +23,9 @@ from ..users import get_user_manager
 
 def create_router() -> APIRouter:
     router = APIRouter(prefix="/auth", tags=["auth"])
-    router.include_router(fastapi_users.get_auth_router(auth_backend))
+    router.include_router(
+        fastapi_users.get_auth_router(auth_backend, requires_verification=True)
+    )
     router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
     router.include_router(fastapi_users.get_reset_password_router())
     router.include_router(fastapi_users.get_verify_router(UserRead))
